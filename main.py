09e127a10f7af113d443da1b619e03b8fc22cf5b
EtienneIzivia <etienne.malacarne@izivia.com>
2023-11-10 10:58:56 +0100

    try

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("main")
+def calculate_sum(a, b):
+    """
+    Calculate and return the sum of two values.
+
+    Parameters:
+    a (int): The first value.
+    b (int): The second value.
+
+    Returns:
+    int: The sum of a and b.
+    """
+    return a + b
+
+# Example usage
+result = calculate_sum(3, 5)
+print("The sum is:", result)
