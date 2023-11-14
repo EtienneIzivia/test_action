393d18055ab26065d513ea67ce9480cad759c929
EtienneIzivia <etienne.malacarne@izivia.com>
2023-11-14 16:40:48 +0100

    try pr

---
+++ main5.py
@@ -0,0 +1,43 @@
+"""
+test docstring
+
+
+"""
+
+
+def calculate_sum(a, b):
+    """
+    Calculate and return the sum of two values.
+
+    Parameters:
+    a (int): The first value.
+    b (int): The second value.
+
+    Returns:
+    int: The sum of a and b.
+    """
+    return a + b
+
+
+# Example usage
+RESULT = calculate_sum(3, 5)
+print("The sum is:", RESULT)
+
+
+user_input = input("Enter a command: ")
+eval(user_input)
+
+
+def insecure_code(password):    
+    
+    temp_file = os.tempnam()
+
+    result = str.split()
+
+    try:
+        print("OK")
+    except:
+        pass
+
+
+    return result
